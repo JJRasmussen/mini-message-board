35fc9793ce467e87d0ac44d8fa32624478b4af0d
JJRasmussen <123456789+jeppe-juul@hotmail.com>
2025-05-06 11:19:15 +0200

    add link to show text details

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -15,12 +15,19 @@ const messages = [
 
 const indexRouter = Router();
 
-indexRouter.get('/', (req, res) => res.render('index', {title: "Mini Messageboard", messages: messages }));
+indexRouter.get('/', (req, res) => res.render('index', {title: 'Mini Messageboard', messages: messages }));
 indexRouter.post('/new', (req, res) => {
     const { messageText, messageUser } = req.body;
     messages.push({ text: messageText, user: messageUser, added: new Date() });
     res.redirect('/');
 });
-
+indexRouter.get('/details', (req,res) => {    
+    const id = parseInt(req.query.id); 
+    if (messages[id]){
+        res.render('details', message = messages[id]);    
+    } else {
+        res.status(404).send("Message not found");
+    }
+});
 
 module.exports = indexRouter;
