8bc24734f084edb10797e8019f622930ca95b3bd
JJRasmussen <123456789+jeppe-juul@hotmail.com>
2025-05-09 13:33:57 +0200

    fix typo

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 console.log('pool says:')
-console.log("db_Name:" + process.env.db_Name)
-console.log("db_Username" + process.env.db_Username)
-console.log("db_Password" +process.env.db_Password)
-console.log("db_Host" +process.env.db_Host)
-console.log("db_Port" +process.env.db_Port)
+console.log("db_Name:" + process.env.DATABASE_NAME)
+console.log("db_Username" + process.env.DATABASE_USERNAME)
+console.log("db_Password" +process.env.DATABASE_PASSWORD)
+console.log("db_Host" +process.env.DATABASE_HOST)
+console.log("db_Port" +process.env.DATABASE_PORT)
 //all the following should be read from environment variables
 module.exports = new Pool({
     host: process.env.DATABASE_HOST, //or whereever the db is hosted
