5bf761ff3e803271303e5ebb0b9f670029117b24
JJRasmussen <123456789+jeppe-juul@hotmail.com>
2025-05-09 12:44:03 +0200

    fix typo and add env details

--- src/db/pool.js
+++ src/db/pool.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 //all the following should be read from environment variables
 module.exports = new Pool({
-    host: DATABASE_HOST, //or whereever the db is hosted
+    host: process.env.DATABASE_HOST, //or whereever the db is hosted
     database: process.env.DATABASE_NAME,
     user: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
